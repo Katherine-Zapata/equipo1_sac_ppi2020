e59226c4ab0bc60130e36a50325082f5bf7e1c2a
KatherineZapat1 <66697330+Katherine-Zapata@users.noreply.github.com>
2020-10-20 19:53:53 +0000

    Cambios del backend

--- backend-jugandoyaprendiendo/index.js
+++ backend-jugandoyaprendiendo/index.js
@@ -1,6 +1,14 @@
 const express = require(`express`)
+const morgan = require("morgan")
 const app = express()
 
+
+//Middelwares
+app.use(morgan('dev'))
+app.use(express.json())
+
+//Routes
+app.use("/api/", require("./routes/estudiantes"))
 
 app.get("/", function (req, res) {
     res.send("Hola, bienvenidos al maravilloso mundo del aprendizaje")
---
+++ backend-jugandoyaprendiendo/routes/estudiantes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express")
+const router = Router()
+const {connection} = require('../db/mysql')
+    
+router.get("/estudiantes", (req, res) => {
+    connection.query('SELECT * FROM estudiantes',  (error, rows, fields) => {
+        if(!error){
+            res.json(rows)
+        }else{
+            res.json({error: "Error ejecutando la consulta"})
+        }
+    })
+})
+
+  
+module.exports = router
