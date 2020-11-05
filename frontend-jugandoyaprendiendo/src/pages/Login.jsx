e5bfee8b118960cea0d3fa98dce4fffa82ac300d
KatherineZapat1 <66697330+Katherine-Zapata@users.noreply.github.com>
2020-11-05 01:37:05 +0000

    Modificación del Frontend- Login

--- frontend-jugandoyaprendiendo/src/pages/Login.jsx
+++ frontend-jugandoyaprendiendo/src/pages/Login.jsx
@@ -15,7 +15,7 @@ class Login extends React.Component {
                     <img src="https://img2.freepng.es/20180331/yjw/kisspng-computer-icons-user-profile-login-user-5abf1426c55235.9485280815224719748082.jpg" width="120" height="100"></img>
                     <br/>
                     <br/>
-                    <form>
+                    <form action="Profesores">
                         <label for="Gmail">Gmail: (Campo Obligatorio)</label>
                         <br />
                         <input type="text" name="Gmail" id="Gmail" required />
